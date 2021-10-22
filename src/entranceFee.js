4f358ba3d95aea3a2f3120ca34b6dcbef465f66b
Colin Beuttler <beutt102@mail.chapman.edu>
2021-10-22 15:16:21 -0700

    update

--- src/entranceFee.js
+++ src/entranceFee.js
@@ -1,7 +1,12 @@
-function FeePrompt() {
-  const confirmPrompt = window.confirm("Are you 18?");
-  return confirmPrompt;
-}
+import React, { Component } from "react";
+
+const FeePrompt = () => {
+  if (window.confirm("Are you 18 or over?")) {
+    alert("Are you sure?");
+  } else {
+    alert("We won't tell anyone ;)");
+  }
+};
 
 // async function doubleConfirm() {
 //   const confirmation = await enteranceFee();
