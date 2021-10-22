cd979011cae5007b5c1b0006eeca368b49d375fc
Colin Beuttler <beutt102@mail.chapman.edu>
2021-10-22 16:45:32 -0700

    compFee

--- src/entranceFee.js
+++ src/entranceFee.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 
-const FeePrompt = () => {
-  if (window.confirm("Are you 18 or over?")) {
-    alert("Are you sure?");
-  } else {
-    alert("We won't tell anyone ;)");
+// const FeePrompt = () => {
+//   if (window.confirm("Are you 18 or over?")) {
+//     alert("Are you sure?");
+//   } else {
+//     alert("We won't tell anyone ;)");
+//   }
+// };
+
+class FeePrompt extends Component {
+  render() {
+    return window.confirm("Are you 18 or over?");
   }
-};
+}
 
 // async function doubleConfirm() {
 //   const confirmation = await enteranceFee();
