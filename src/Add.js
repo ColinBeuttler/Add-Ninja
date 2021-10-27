a323c6a56b051eead572dae0c9ca12cf1a011240
Colin Beuttler <beutt102@mail.chapman.edu>
2021-10-26 18:34:17 -0700

    update

---
+++ src/Add.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class Add extends Component {
+  render() {
+    return (
+      <div className="addBox">
+        <h1>Cool-Aid 50% Off</h1>
+        <p>
+          When you buy a large monster, at least three hotdogs, and a pack of
+          mints. Not Refunadable, only applies to 1001 pack
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Add;
